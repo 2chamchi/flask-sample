57ec9a777e8fff6afd6303fccd0d9b00f16e4d2e
광포한까마귀 <2chamchi@gmail.com>
2020-10-15 21:22:34 +0900

    3-02 페이징

--- pybo/views/question_views.py
+++ pybo/views/question_views.py
@@ -22,7 +22,9 @@ def create():
 
 @bp.route('/list/')
 def _list():
+    page = request.args.get('page', type=int, default=1)  # 페이지
     question_list = Question.query.order_by(Question.create_date.desc())
+    question_list = question_list.paginate(page, per_page=10)
     return render_template('question/question_list.html', question_list=question_list)
 
 
