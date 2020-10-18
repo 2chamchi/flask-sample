61ca18ed57d25fc4a0bf7f94c53bbdaf453e1d89
광포한까마귀 <2chamchi@gmail.com>
2020-10-18 16:51:15 +0900

    3-12 추천   . vote_views.py 생성

--- pybo/models.py
+++ pybo/models.py
@@ -1,6 +1,20 @@
 from pybo import db
 
 
+question_voter = db.Table(
+    'question_voter',
+    db.Column('user_id', db.Integer, db.ForeignKey('user.id', ondelete='CASCADE'), primary_key=True),
+    db.Column('question_id', db.Integer, db.ForeignKey('question.id', ondelete='CASCADE'), primary_key=True)
+)
+
+
+answer_voter = db.Table(
+    'answer_voter',
+    db.Column('user_id', db.Integer, db.ForeignKey('user.id', ondelete='CASCADE'), primary_key=True),
+    db.Column('answer_id', db.Integer, db.ForeignKey('answer.id', ondelete='CASCADE'), primary_key=True)
+)
+
+
 class Question(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     subject = db.Column(db.String(200), nullable=False)
@@ -9,6 +23,7 @@ class Question(db.Model):
     modify_date = db.Column(db.DateTime(), nullable=True)
     user_id = db.Column(db.Integer, db.ForeignKey('user.id', ondelete='CASCADE'), nullable=False)
     user = db.relationship('User', backref=db.backref('question_set'))
+    voter = db.relationship('User', secondary=question_voter, backref=db.backref('question_voter_set'))
 
 
 class Answer(db.Model):
@@ -20,6 +35,7 @@ class Answer(db.Model):
     modify_date = db.Column(db.DateTime(), nullable=True)
     user_id = db.Column(db.Integer, db.ForeignKey('user.id', ondelete='CASCADE'), nullable=False)
     user = db.relationship('User', backref=db.backref('answer_set'))
+    voter = db.relationship('User', secondary=answer_voter, backref=db.backref('answer_voter_set'))
 
 
 class User(db.Model):
