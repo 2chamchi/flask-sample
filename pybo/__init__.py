c0b0c1ded7066558c80dbe9a3990bf3251e855b9
광포한까마귀 <2chamchi@gmail.com>
2020-10-18 17:01:05 +0900

    3-14 마크다운

--- pybo/__init__.py
+++ pybo/__init__.py
@@ -2,6 +2,7 @@ from flask import Flask
 from flask_migrate import Migrate
 from flask_sqlalchemy import SQLAlchemy
 from sqlalchemy import MetaData
+from flaskext.markdown import Markdown
 
 import config
 
@@ -42,6 +43,8 @@ def create_app():
 
     from .filter import format_datetime
     app.jinja_env.filters['datetime'] = format_datetime
+
+    Markdown(app, extensions=['nl2br', 'fenced_code'])
 
     return app
 
